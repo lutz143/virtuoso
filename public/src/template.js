041a213a6db1b0f088693d3302365846783c3f90
Josh <lutz_143@yahoo.com>
2023-02-19 21:00:36 -0500

    updates to test visuals to be implemented into handlebars

--- public/src/template.js
+++ public/src/template.js
@@ -3,3 +3,21 @@ $(document).ready(function() {
   $("#sortable").sortable();
        $("#sortable").disableSelection();
 });
+
+// unhide the search input bar after user selects search from sidebar
+$(".hamburger").on("click", function(){
+  const middleSectionEl = document.querySelector('.nav-middle-section');
+  const rightSectionEl = document.querySelector('.nav-right-section');
+  // const hideEl = document.querySelector('.hide');
+
+  if(middleSectionEl.style.display !=="none") {
+    middleSectionEl.style.display = "none";
+    rightSectionEl.style.display = "none";
+    // hideEl.style.display = "none";
+  } else {
+    middleSectionEl.style.display = "flex";
+    rightSectionEl.style.display = "flex";
+    // hideEl.style.display = "flex";
+    // searchBarEl.focus();
+  }
+})
--- public_test/src/template.js
+++ public_test/src/template.js
@@ -3,3 +3,46 @@ $(document).ready(function() {
   $("#sortable").sortable();
        $("#sortable").disableSelection();
 });
+
+// unhide the search input bar after user selects search from sidebar
+$(".hamburger").on("click", function(){
+  const homeButtonEl = document.querySelector('#home-button');
+  const middleSectionEl = document.querySelector('.nav-middle-section');
+  const rightSectionEl = document.querySelector('.nav-right-section');
+  // const hideEl = document.querySelector('.hide');
+
+  if(homeButtonEl.style.display !=="none") {
+    homeButtonEl.style.display = "none";
+    // rightSectionEl.style.display = "none";
+    // hideEl.style.display = "none";
+  } else {
+    homeButtonEl.style.transition = "all 0.25s ease-in";
+    window.setTimeout(function(){
+      homeButtonEl.style.display = "inline";
+    },500); //timed to match animation duration
+    // rightSectionEl.style.display = "block";
+    // hideEl.style.display = "flex";
+    // searchBarEl.focus();
+  }
+})
+
+// $(".hamburger").on("click", function(){
+//   const navOptionsEl = document.querySelector('.nav-options');
+//   const middleSectionEl = document.querySelector('.nav-middle-section');
+//   const rightSectionEl = document.querySelector('.nav-right-section');
+//   // const hideEl = document.querySelector('.hide');
+
+//   if(middleSectionEl.style.display !=="none") {
+//     middleSectionEl.style.display = "none";
+//     rightSectionEl.style.display = "none";
+//     // hideEl.style.display = "none";
+//   } else {
+//     window.setTimeout(function(){
+//       // navOptionsEl.style.transition = "all 0.25s ease-in";
+//       navOptionsEl.style.display = "flex";
+//     },100); //timed to match animation duration
+//     rightSectionEl.style.display = "block";
+//     // hideEl.style.display = "flex";
+//     // searchBarEl.focus();
+//   }
+// })
